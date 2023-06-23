86b75ef5cac5362feccd9d9ff241acdb417e1a98
tanmoy <pdt.tanmoy@gmail.com>
2023-06-24 00:02:58 +0600

    CityService class added to get CityRepository data

--- src/main/java/com/tanmoy/weatherapi/repository/CityRepository.java
+++ src/main/java/com/tanmoy/weatherapi/repository/CityRepository.java
@@ -4,4 +4,6 @@ import com.tanmoy.weatherapi.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    City findByCityNameAndCountryName(String cityName, String countryName);
 }
